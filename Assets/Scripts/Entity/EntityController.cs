081ce559311ecdb5075356b461c05773fa191fff
ThePaper <the.paper@live.co.kr>
2022-09-04 00:11:05 +0900

    캐릭터 이동 구현  - Input System 으로 수정

--- Assets/Scripts/Entity/EntityController.cs
+++ Assets/Scripts/Entity/EntityController.cs
@@ -6,37 +6,21 @@ public class EntityController : MonoBehaviour
 {
     Animator m_Animator;
     bool m_IsWalking = false;
+    Vector2 m_MoveDir;
 
     // Start is called before the first frame update
     void Start()
     {
         m_Animator = GetComponent<Animator>();
+        InitPlayerInput(GetComponent<UnityEngine.InputSystem.PlayerInput>());
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = Vector3.zero;
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (m_MoveDir.sqrMagnitude > 0f)
         {
-            dir += Vector3.left;
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            dir += Vector3.right;
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            dir += Vector3.forward;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            dir += Vector3.back;
-        }
-
-        if (dir.sqrMagnitude > 0f)
-        {
-            transform.rotation = Quaternion.LookRotation(Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * dir);
+            transform.rotation = Quaternion.LookRotation(Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * new Vector3(m_MoveDir.x, 0f, m_MoveDir.y));
             transform.Translate(Vector3.forward * 2.0f * Time.deltaTime);
 
             if (m_IsWalking == false)
@@ -54,4 +38,53 @@ public class EntityController : MonoBehaviour
             m_IsWalking = false;
         }
     }
+
+
+    #region Input System
+
+    protected UnityEngine.InputSystem.PlayerInput m_PlayerInput;
+    protected UnityEngine.InputSystem.InputAction m_InputAction_Move;
+
+    public void InitPlayerInput(UnityEngine.InputSystem.PlayerInput playerInput)
+    {
+        if (m_PlayerInput == playerInput)
+            return;
+
+        //이전 액션에 콜백 제거
+        if (m_InputAction_Move != null)
+        {
+            m_InputAction_Move.started -= OnInput_Move;
+            m_InputAction_Move.canceled -= OnInput_Move;
+            m_InputAction_Move.performed -= OnInput_Move;
+            m_InputAction_Move = null;
+        }
+
+        m_PlayerInput = playerInput;
+        if (m_PlayerInput != null)
+        {
+            m_InputAction_Move = m_PlayerInput.currentActionMap.FindAction("Move");
+        }
+
+        //신규 액션에 콜백 등록
+        if (m_InputAction_Move != null)
+        {
+            m_InputAction_Move.started += OnInput_Move;
+            m_InputAction_Move.canceled += OnInput_Move;
+            m_InputAction_Move.performed += OnInput_Move;
+        }
+    }
+
+    void OnInput_Move(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        if (context.valueType == null)
+        {
+            m_MoveDir = Vector2.zero;
+        }
+        else
+        {
+            m_MoveDir = context.ReadValue<Vector2>();
+        }
+    }
+
+    #endregion
 }
