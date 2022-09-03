a6e59e62778aacc2db6688a1a6ac96df5234c8d9
ThePaper <the.paper@live.co.kr>
2022-09-03 15:43:54 +0900

    캐릭터 이동 구현  - 일단 움직이게 구현함

--- Assets/Scripts/Entity/EntityController.cs
+++ Assets/Scripts/Entity/EntityController.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class EntityController : MonoBehaviour
 {
+    Animator m_Animator;
+    bool m_IsWalking = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 dir = Vector3.zero;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            dir += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            dir += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            dir += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            dir += Vector3.back;
+        }
 
+        if (dir.sqrMagnitude > 0f)
+        {
+            transform.rotation = Quaternion.LookRotation(Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * dir);
+            transform.Translate(Vector3.forward * 2.0f * Time.deltaTime);
+
+            if (m_IsWalking == false)
+            {
+                m_Animator.Play("Walk");
+            }
+            m_IsWalking = true;
+        }
+        else
+        {
+            if (m_IsWalking == true)
+            {
+                m_Animator.Play("Idle");
+            }
+            m_IsWalking = false;
+        }
     }
 }
