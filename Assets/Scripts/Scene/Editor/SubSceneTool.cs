012a363b237a130dfd20b2e10671258f614b42aa
이성희 <the.paper@live.co.kr>
2022-09-10 19:15:29 +0900

    서브씬 설정 툴 개발
     - 메인씬, 세팅 설정 기능 추가

--- Assets/Scripts/Scene/Editor/SubSceneTool.cs
+++ Assets/Scripts/Scene/Editor/SubSceneTool.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 
 public class SubSceneTool : EditorWindow
 {
@@ -13,12 +15,70 @@ public class SubSceneTool : EditorWindow
     }
 
 
+    public bool IsAutoSelectMainScene
+    {
+        set { EditorPrefs.SetBool("SubSceneTool_IsAutoSelectMainScene", value); }
+        get { return EditorPrefs.GetBool("SubSceneTool_IsAutoSelectMainScene", true); }
+    }
+
+    public SceneAsset SelectedMainScene = null;
+    public SubSceneSetting SelectedSubSceneSetting = null;
     public Vector2 SubSceneScoll = Vector2.zero;
+
+    private void OnEnable()
+    {
+        EditorSceneManager.sceneOpened += OnSceneOpened;
+    }
+
+    private void OnDisable()
+    {
+        EditorSceneManager.sceneOpened -= OnSceneOpened;
+    }
+
+    private void OnSceneOpened(Scene scene, OpenSceneMode mode)
+    {
+        if (IsAutoSelectMainScene == true && mode == OpenSceneMode.Single)
+            AutoSelectMainScene();
+    }
 
     private void OnGUI()
     {
-        //TODO - 메인씬 설정 기능
-        //TODO - 서브씬 세팅 파일 생성/변경
+        var isAutoSelectMainScene = EditorGUILayout.Toggle("Auto Select", IsAutoSelectMainScene);
+        if (isAutoSelectMainScene != IsAutoSelectMainScene)
+        {
+            IsAutoSelectMainScene = isAutoSelectMainScene;
+            if (IsAutoSelectMainScene)
+                AutoSelectMainScene();
+        }
+        using (new EditorGUI.DisabledScope(IsAutoSelectMainScene == true))
+        {
+            var selectedMainScene = (SceneAsset)EditorGUILayout.ObjectField("Main Scene", SelectedMainScene, typeof(SceneAsset), false);
+            if (selectedMainScene != SelectedMainScene)
+            {
+                SelectedMainScene = selectedMainScene;
+                OnChangeSelectedMainScene();
+            }
+        }
+
+        if (SelectedSubSceneSetting == null)
+        {
+            using (new EditorGUI.DisabledScope(SelectedMainScene == null))
+            {
+                if (GUILayout.Button("Create Setting"))
+                {
+                    var setting = SubSceneSetting.GenerateSubSceneSetting(SelectedMainScene.name);
+                    SelectedSubSceneSetting = setting;
+                }
+            }
+        }
+        else
+        {
+            using (new EditorGUI.DisabledScope(true))
+            {
+                EditorGUILayout.ObjectField("Setting", SelectedSubSceneSetting, typeof(SubSceneSetting), false);
+            }
+        }
+        EditorGUILayout.Space();
 
         SubSceneScoll = EditorGUILayout.BeginScrollView(SubSceneScoll);
         OnGUI_SubScenes();
@@ -32,5 +92,29 @@ public class SubSceneTool : EditorWindow
         //TODO - SubSceneData, 씬 등록
         //TODO - SubSceneData, 씬 로드, 언로드, 세이브
         //TODO - SubSceneData, 중심, 거리 계산
+    }
+
+    private void AutoSelectMainScene()
+    {
+        for (int i = 0; i < UnityEditor.SceneManagement.EditorSceneManager.sceneCount; i ++)
+        {
+            var sceneData = UnityEditor.SceneManagement.EditorSceneManager.GetSceneAt(i);
+            if (sceneData.IsValid() == false) continue;
+
+            SelectedMainScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(sceneData.path);
+            OnChangeSelectedMainScene();
+            break;
+        }
+    }
+
+    private void OnChangeSelectedMainScene()
+    {
+        if (SelectedMainScene == null)
+        {
+            SelectedMainScene = null;
+            return;
+        }
+
+        SelectedSubSceneSetting = AssetDatabase.LoadAssetAtPath<SubSceneSetting>($"Assets/Resources/SubSceneSettings/{SelectedMainScene.name}.asset");
     }
 }
